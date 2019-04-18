8b201d2be7f5d35be183aa5205c035118bdc6abc
StephenBurke <42814417+StephenBurke@users.noreply.github.com>
2019-04-18 13:22:00 -0500

    sorted different radius into lists

--- tSP.py
+++ tSP.py
@@ -12,6 +12,18 @@ with open("holeData.csv") as csvfile:
     yCoord = []
     radius = []
 
+    x1875 = []
+    y1875 = []
+    x25 = []
+    y25 = []
+    x3125 = []
+    y3125 = []
+    x375 = []
+    y375 = []
+    x4375 = []
+    y4375 = []
+    x5 = []
+    y5 = []
 
     for row in readCSV:
 
@@ -19,11 +31,38 @@ with open("holeData.csv") as csvfile:
         y = float(row[1])
         r = float(row[2])
 
+        if(r==0.1875):
+            x1875.append(x)
+            y1875.append(y)
+            plt.plot(x1875, y1875, 'r.')
+            #radius.append(r)
+        elif(r==0.25):
+            x25.append(x)
+            y25.append(y)
+            plt.plot(x25, y25, 'b.')
+        elif(r==0.3125):
+            x3125.append(x)
+            y3125.append(y)
+            plt.plot(x3125, y3125, 'g.')
+        elif(r==0.375):
+            x375.append(x)
+            y375.append(y)
+            plt.plot(x375, y375, 'y.')
+        elif(r==0.4375):
+            x4375.append(x)
+            y4375.append(y)
+            plt.plot(x4375, y4375, 'g*')
+        else:
+            x5.append(x)
+            y5.append(y)
+            plt.plot(x5, y5, 'b*')
+        
+        
         
     
-        xCoord.append(x)
-        yCoord.append(y)
-        radius.append(r)
+        #xCoord.append(x)
+        #yCoord.append(y)
+        #radius.append(r)
 
-    plt.plot(xCoord,yCoord,'bo')
+    #plt.plot(xCoord,yCoord,'bo')
     plt.show()
